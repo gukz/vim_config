3e2295be443b22a6e13037fc5b44d876849b5c29
Gang Wang <wanggang@microsoft.com>
2021-05-23 20:46:25 +0800

    Learned from LunarVim configs, all change to lua config

---
+++ lua/keymappings.lua
@@ -0,0 +1,65 @@
+vim.api.nvim_set_keymap('n', '-', ':RnvimrToggle<CR>', {noremap = true, silent = true})
+
+-- cr auto clean highlights
+vim.api.nvim_set_keymap('n', '<cr>', ':noh<cr><cr>', {noremap = true, silent = true})
+
+-- = auto redraw the layout
+vim.api.nvim_set_keymap('n', '=', '<c-w>=', {noremap = true, silent = true})
+
+-- better window movement
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', {silent = true})
+
+-- Terminal window navigation
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<C-h>', '<C-\\><C-N><C-w>h', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<C-j>', '<C-\\><C-N><C-w>j', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<C-k>', '<C-\\><C-N><C-w>k', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<C-l>', '<C-\\><C-N><C-w>l', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-h>', '<C-\\><C-N><C-w>h', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-j>', '<C-\\><C-N><C-w>j', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-k>', '<C-\\><C-N><C-w>k', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<C-l>', '<C-\\><C-N><C-w>l', {noremap = true, silent = true})
+
+-- resize with arrows
+vim.api.nvim_set_keymap('n', '<C-Up>', ':resize -2<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-Down>', ':resize +2<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-Left>', ':vertical resize -2<CR>', {silent = true})
+vim.api.nvim_set_keymap('n', '<C-Right>', ':vertical resize +2<CR>', {silent = true})
+
+-- better indenting
+vim.api.nvim_set_keymap('v', '<', '<gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '>', '>gv', {noremap = true, silent = true})
+
+-- I hate escape
+vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'kj', '<ESC>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {noremap = true, silent = true})
+
+-- Tab switch buffer
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', {noremap = true, silent = true})
+
+-- Open current file in new tab
+-- vim.api.nvim_set_keymap('n', '<leader>o', ':tabnew expand("%:p")', {noremap = true, silent = true})
+
+-- Move selected line / block of text in visual mode
+vim.api.nvim_set_keymap('x', 'K', ':move \'<-2<CR>gv-gv', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('x', 'J', ':move \'>+1<CR>gv-gv', {noremap = true, silent = true})
+
+-- Better nav for omnicomplete
+vim.api.nvim_set_keymap('i', '<C-j>', '(\"\\<C-n>\")', {noremap = true, expr = true})
+vim.api.nvim_set_keymap('i', '<C-k>', '(\"\\<C-p>\")', {noremap = true, expr = true})
+-- vim.cmd('inoremap <expr> <TAB> (\"\\<C-n>\")')
+-- vim.cmd('inoremap <expr> <S-TAB> (\"\\<C-p>\")')
+
+-- vim.api.nvim_set_keymap('i', '<C-TAB>', 'compe#complete()', {noremap = true, silent = true, expr = true})
+
+-- vim.cmd([[
+-- map p <Plug>(miniyank-autoput)
+-- map P <Plug>(miniyank-autoPut)
+-- map <leader>n <Plug>(miniyank-cycle)
+-- map <leader>N <Plug>(miniyank-cycleback)
+-- ]])
---
+++ lua/lv-globals.lua
@@ -0,0 +1,50 @@
+O = {
+    python = {
+        linter = 'black',
+        -- @usage can be 'yapf', 'black'
+        formatter = 'yapf',
+        autoformat = true,
+        isort = true,
+        diagnostics = {virtual_text = true, signs = false, underline = true},
+		analysis = {type_checking = "off", auto_search_paths = true, use_library_code_types = true}
+    },
+    dart = {sdk_path = '/usr/lib/dart/bin/snapshots/analysis_server.dart.snapshot'},
+    lua = {
+        -- @usage can be 'lua-format'
+        formatter = 'lua-format',
+        autoformat = false,
+        diagnostics = {virtual_text = {spacing = 0, prefix = ""}, signs = false, underline = true}
+    },
+    sh = {
+        -- @usage can be 'shellcheck'
+        linter = 'shellcheck',
+        -- @usage can be 'shfmt'
+        formatter = 'shfmt',
+        autoformat = false,
+        diagnostics = {virtual_text = {spacing = 0, prefix = ""}, signs = false, underline = true}
+    },
+    tsserver = {
+        -- @usage can be 'eslint'
+        linter = '',
+        -- @usage can be 'prettier'
+        formatter = 'prettier',
+        autoformat = true,
+        diagnostics = {virtual_text = {spacing = 0, prefix = ""}, signs = false, underline = true}
+    },
+    json = {
+        -- @usage can be 'prettier'
+        formatter = 'prettier',
+        autoformat = true,
+        diagnostics = {virtual_text = {spacing = 0, prefix = ""}, signs = false, underline = true}
+    },
+    tailwindls = {filetypes = {'html', 'css', 'scss', 'javascript', 'javascriptreact', 'typescript', 'typescriptreact'}},
+    clang = {diagnostics = {virtual_text = false, signs = false, underline = false}},
+	ruby = {
+        autoformat = true,
+		diagnostics = {virtualtext = {spacing = 0, prefix = ""}, signs = false, underline = true},
+		filetypes = {'rb', 'erb', 'rakefile'}
+	}
+}
+
+DATA_PATH = vim.fn.stdpath('data')
+CACHE_PATH = vim.fn.stdpath('cache')
